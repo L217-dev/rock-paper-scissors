4d821c64b8c440d4c00eb77b1c0f8e9dfc59e216
LFG <198674398+L217-dev@users.noreply.github.com>
2025-03-12 22:32:23 -0500

    Added the function getHumanChoice -Created a variable HumanChoice a get the choice using a window.prompt

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,10 +1,16 @@
 console.log('time to play')
 
 function getComputerChoice(){
+    // this fuction create this options for the computer and select one using rando numbers between 0 and 2
     let options = ['rock','paper','scissors']
     let computerChoice=options[Math.floor(Math.random()*3)]
     return computerChoice
-
+//this function could be make it whit using if , if else and else to select the rock, paper or scissor 
 }
 
-console.log(getComputerChoice())
+function getHumanChoice(){
+    let HumanChoice=window.prompt('Select one of these rock, paper, scissors right now this game is cases sensitve')
+ return HumanChoice
+ // this function get the human choice using a prompt window
+}
+
