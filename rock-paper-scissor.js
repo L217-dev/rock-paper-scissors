f61b4f1fdb7e92abd7ba248b81173ac9bd06d1ab
LFG <198674398+L217-dev@users.noreply.github.com>
2025-03-12 22:35:54 -0500

    Create two new variable: -Created HumanScore and ComputerScore -Scores variables initialize in 0

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,16 +1,17 @@
 console.log('time to play')
-
+let HumanScore=0
+let ComputerScore=0
 function getComputerChoice(){
     // this fuction create this options for the computer and select one using rando numbers between 0 and 2
-    let options = ['rock','paper','scissors']
-    let computerChoice=options[Math.floor(Math.random()*3)]
-    return computerChoice
+    let options = ['rock','paper','scissors'];
+    let computerChoice=options[Math.floor(Math.random()*3)];
+    return computerChoice;
 //this function could be make it whit using if , if else and else to select the rock, paper or scissor 
 }
 
 function getHumanChoice(){
-    let HumanChoice=window.prompt('Select one of these rock, paper, scissors right now this game is cases sensitve')
- return HumanChoice
+    let HumanChoice=window.prompt('Select one of these rock, paper, scissors right now this game is cases sensitve');
+ return HumanChoice;
  // this function get the human choice using a prompt window
 }
 
