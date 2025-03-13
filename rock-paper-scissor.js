7bbd315d4d2f479b64ae84f5732bed075c415f20
LFG <198674398+L217-dev@users.noreply.github.com>
2025-03-12 23:25:51 -0500

    Implement game logic for Rock Paper Scissors:
    Created playRound:
    - Create a playRound function that accepts two parameters: humanChoice and computerChoice.
    -humanChoice is case-insensitive to and don't accept inputs like 'Rock', 'ROCK', etc. accept inputs[rock,paper,scissors]
    - Compare the human and computer choices, log a result message whit the result of the round and the scores
    - Increment the humanScore or computerScore variable based on the round winner.
    
    Created playGame:
    - Create a playGame function that encapsulates the game logic.
    - playRound is inside of playgame and is calling 5 time.
    - Track scores over rounds and declare a final winner at the end.

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,17 +1,66 @@
 console.log('time to play')
-let HumanScore=0
-let ComputerScore=0
+let HumanScore=0;
+let ComputerScore=0;
 function getComputerChoice(){
     // this fuction create this options for the computer and select one using rando numbers between 0 and 2
-    let options = ['rock','paper','scissors'];
-    let computerChoice=options[Math.floor(Math.random()*3)];
+    const options = ['rock','paper','scissors'];
+    const computerChoice=options[Math.floor(Math.random()*3)];
     return computerChoice;
 //this function could be make it whit using if , if else and else to select the rock, paper or scissor 
 }
 
 function getHumanChoice(){
-    let HumanChoice=window.prompt('Select one of these rock, paper, scissors right now this game is cases sensitve');
+    const HumanChoice=window.prompt('Select one of these rock, paper, scissors right now this game is cases sensitve');
  return HumanChoice;
  // this function get the human choice using a prompt window
 }
 
+function playGame(){
+    let HumanScore=0;
+    let ComputerScore=0;
+    function playRound(humanChoice,computerChoice){
+        if (humanChoice===computerChoice){
+            return console.log(`This is Draw the score is: you ${HumanScore}, Computer ${ComputerScore}`);
+        }
+        else if ((humanChoice==='rock' && computerChoice==='scissors') || (humanChoice==='paper' && computerChoice==='rock') || (humanChoice==='scissors' && computerChoice==='paper') ) {
+            HumanScore++;
+            return console.log(`This is Win the score is: you ${HumanScore}, Computer ${ComputerScore}`)   ;
+        } else {
+            ComputerScore++;
+            return console.log(`This is Losses the score is: you ${HumanScore}, Computer ${ComputerScore}`);
+    
+        }
+    
+    }
+
+    let humanSelection=getHumanChoice();
+    let computerSelection=getComputerChoice();
+    playRound(humanSelection,computerSelection)
+    
+    humanSelection=getHumanChoice();
+    computerSelection=getComputerChoice()
+    playRound(humanSelection,computerSelection)
+    
+    humanSelection=getHumanChoice();
+    computerSelection=getComputerChoice()
+    playRound(humanSelection,computerSelection)
+
+    humanSelection=getHumanChoice();
+    computerSelection=getComputerChoice()
+    playRound(humanSelection,computerSelection)
+
+    humanSelection=getHumanChoice();
+    computerSelection=getComputerChoice()
+    playRound(humanSelection,computerSelection)
+
+    if (HumanScore===ComputerScore){
+        console.log(`This is Draw game the score is: you ${HumanScore}, Computer ${ComputerScore}`)
+    }
+    else if (HumanScore>ComputerScore){
+        console.log(`You win the game  the score is: you ${HumanScore}, Computer ${ComputerScore}`)
+    } else{
+        console.log(`You Losses the game  the score is: you ${HumanScore}, Computer ${ComputerScore}`)
+    }
+}
+
+playGame()
