42257f4c970b87b3c2f87f2ce0496bed769c4de6
LFG <198674398+L217-dev@users.noreply.github.com>
2025-03-24 20:47:54 -0500

    enhancent UI/UX: -add h2 board discription -change the scrip to show to the player the computer choose -change some text to show to the player -add a queryselector to h2 to modiied the text -add style to h2

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -10,32 +10,35 @@ const playScissor=document.querySelector('#scissor')
 const playPaper=document.querySelector('#paper')
 const boardPlayer=document.querySelector("#playerScore")
 const boardComputer=document.querySelector("#computerScore")
+const matchDescription=document.querySelector('h2')
 
 function getComputerChoice(){
     // this fuction create this options for the computer and select one using rando numbers between 0 and 2
     const options = ['rock','paper','scissor'];
     const computerChoice=options[Math.floor(Math.random()*3)];
     return computerChoice;
-//this function could be make it whit using if , if else and else to select the rock, paper or scissor 
+
 }
     
 function playRound(playerSelection){
     const computerChoice=getComputerChoice();
     gameResult.textContent=''
+    matchDescription.textContent='Match Score'
     if (playerSelection===computerChoice){
-        result.textContent=`This is Draw the score is: you ${humanScore}, Computer ${computerScore}`
+        result.textContent=`Draw both choose ${computerChoice}: Points: you ${humanScore} - Computer ${computerScore}`;
         }
     else if ((playerSelection==='rock' && computerChoice==='scissor') || (playerSelection==='paper' && computerChoice==='rock') || (playerSelection==='scissor' && computerChoice==='paper') ) {
         humanScore++;
-        result.textContent=`This is Win the score is: you ${humanScore}, Computer ${computerScore}`  ;
+        result.textContent=`You Win the computer choose ${computerChoice}: Points: you ${humanScore} - Computer ${computerScore}`;
     } else {
         computerScore++;
-        result.textContent=`This is Losses the score is: you ${humanScore}, Computer ${computerScore}`;
+        result.textContent=`You Losses the computer choose ${computerChoice}: Points: you ${humanScore} - Computer ${computerScore}`;
     }
     
     function whoWin(){
     if (humanScore===5){
         gameResult.textContent='You win, choose again to play another macht'
+        matchDescription.textContent='Match Score (Firs to get 5 points get 1 score)'
         boardPlayerCount++
         boardPlayer.textContent=boardPlayerCount
         humanScore=0;
@@ -43,6 +46,7 @@ function playRound(playerSelection){
     }
     else if ( computerScore==5){
         gameResult.textContent='Computer win, choose again to play another macht'
+        matchDescription.textContent='Match Score (Firs to get 5 points get 1 score)'
         boardComputerCount++
         boardComputer.textContent=boardComputerCount
         humanScore=0;
